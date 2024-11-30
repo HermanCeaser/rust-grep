5e95b9834ac4a4e102a698ba354f56a94c40ef0f
Herman Ceaser <ceaserbanks09@gmail.com>
2024-11-30 12:37:38 +0300

    refactored code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-use std::{env, fs};
+use std::{env, process};
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("in file '{}'", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should be able to read from file!");
-    println!("With text:\n--------------------------\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query: String = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path}
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
